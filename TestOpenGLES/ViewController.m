e4de5c6a1af11edae39c16e9e9c0ac412e76f61b
ToolMaker <yupeiliang110@gmail.com>
2019-12-25 08:55:49 +0800

    up down left right

--- TestOpenGLES/ViewController.m
+++ TestOpenGLES/ViewController.m
@@ -15,8 +15,8 @@ typedef struct {
 
 static const SceneVertex vertices[] = {
     {{-0.5f,-0.5f,0.0},{0.0f,0.0f}},
-    {{0.5f,-0.5f,0.0},{1.0f,0.0f}},
-    {{-0.5f,0.5f,0.0},{0.0f,1.0f}}
+    {{0.5f,-0.5f,0.0},{2.0f,0.0f}},
+    {{-0.5f,0.5f,0.0},{0.0f,3.0f}}
 };
 
 @interface ViewController ()
