0428fbe21de7eb2da7af5107de7f479391169b38
ToolMaker <yupeiliang110@gmail.com>
2020-01-21 12:32:59 +0800

    显示纹理

--- TestOpenGLES/ViewController.m
+++ TestOpenGLES/ViewController.m
@@ -25,29 +25,25 @@ typedef struct {
 SceneVertex;
 
 
-/////////////////////////////////////////////////////////////////
-// Define vertex data for triangles to use in example
+// Define vertex data for a triangle to use in example
 static const SceneVertex vertices[] =
 {
-   {{-1.0f, -0.67f, 0.0f}, {0.0f, 0.0f}},  // first triangle
-   {{ 1.0f, -0.67f, 0.0f}, {1.0f, 0.0f}},
-   {{-1.0f,  0.67f, 0.0f}, {0.0f, 1.0f}},
-   {{ 1.0f, -0.67f, 0.0f}, {1.0f, 0.0f}},  // second triangle
-   {{-1.0f,  0.67f, 0.0f}, {0.0f, 1.0f}},
-   {{ 1.0f,  0.67f, 0.0f}, {1.0f, 1.0f}},
+   {{-0.5f, -0.5f, 0.0f}, {0.0f, 0.0f}}, // lower left corner
+   {{ 0.5f, -0.5f, 0.0f}, {1.0f, 0.0f}}, // lower right corner
+   {{-0.5f,  0.5f, 0.0f}, {0.0f, 1.0f}}, // upper left corner
 };
 
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    textureScaleFactor = 1.5f;
-    textureAngle = 90.0f;
-    self.textureMatrixStack = GLKMatrixStackCreate(kCFAllocatorDefault);
+//    textureScaleFactor = 1.5f;
+//    textureAngle = 90.0f;
+//    self.textureMatrixStack = GLKMatrixStackCreate(kCFAllocatorDefault);
     
     GLKView *view = (GLKView *)self.view;
     NSAssert([view isKindOfClass:[GLKView class]], @"View controller's view is not a GLKView");
     
-    view.context = [[AGLKContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    view.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
     [EAGLContext setCurrentContext:view.context];
     
     glGenBuffers(1, &vertexBufferID);
@@ -56,7 +52,7 @@ static const SceneVertex vertices[] =
     
     self.baseEffect = [[AGLKTextureTransformBaseEffect alloc] init];
     self.baseEffect.useConstantColor = GL_TRUE;
-    self.baseEffect.constantColor = GLKVector4Make(0.0f, 0.0f, 0.0f, 1.0f);
+    self.baseEffect.constantColor = GLKVector4Make(1.0f, 0.0f, 0.0f, 1.0f);
     
     
     // Setup texture0
@@ -80,37 +76,37 @@ static const SceneVertex vertices[] =
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect {
     //arg4 用于区分颜色， 0 Blue, 1 Green, 2 Purple, 3 Orange, 4 Red
     kdebug_signpost_start(10, 0, 0, 0, 1);
-    
     [self.baseEffect prepareToDraw];
+    
     glClear(GL_COLOR_BUFFER_BIT);
     
     glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
     glEnableVertexAttribArray(GLKVertexAttribPosition);
-    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), NULL+0);
+    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(SceneVertex), NULL+offsetof(SceneVertex, positionCoords));
     
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
     glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
-    glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, 2*sizeof(GLfloat), NULL+offsetof(SceneVertex, textureCoords));
+    glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, sizeof(SceneVertex), NULL+offsetof(SceneVertex, textureCoords));
     
-    GLKMatrixStackPush(self.textureMatrixStack);
-     
-        // Scale and rotate about the center of the texture
-        GLKMatrixStackTranslate(
-           self.textureMatrixStack,
-           0.5, 0.5, 0.0);
-        GLKMatrixStackScale(
-           self.textureMatrixStack,
-           textureScaleFactor, textureScaleFactor, 1.0);
-        GLKMatrixStackRotate(   // Rotate about Z axis
-           self.textureMatrixStack,
-           GLKMathDegreesToRadians(textureAngle),
-           0.0, 0.0, 1.0);
-        GLKMatrixStackTranslate(
-           self.textureMatrixStack,
-           -0.5, -0.5, 0.0);
+//    GLKMatrixStackPush(self.textureMatrixStack);
+//
+//        // Scale and rotate about the center of the texture
+//        GLKMatrixStackTranslate(
+//           self.textureMatrixStack,
+//           0.5, 0.5, 0.0);
+//        GLKMatrixStackScale(
+//           self.textureMatrixStack,
+//           textureScaleFactor, textureScaleFactor, 1.0);
+//        GLKMatrixStackRotate(   // Rotate about Z axis
+//           self.textureMatrixStack,
+//           GLKMathDegreesToRadians(textureAngle),
+//           0.0, 0.0, 1.0);
+//        GLKMatrixStackTranslate(
+//           self.textureMatrixStack,
+//           -0.5, -0.5, 0.0);
         
-        self.baseEffect.textureMatrix2d0 =
-           GLKMatrixStackGetMatrix4(self.textureMatrixStack);
-    
+//    self.baseEffect.textureMatrix2d0 = GLKMatrixStackGetMatrix4(self.textureMatrixStack);
+
     glDrawArrays(GL_TRIANGLES, 0, sizeof(vertices)/sizeof(SceneVertex));
     
     kdebug_signpost_end(10, 0, 0, 0, 1);
